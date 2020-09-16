2bd77467ad6ff2740467dac0062f86105e75901d
Cheea9337 <cheea9337@wrdsb.ca>
2020-09-16 12:54:37 +0000

    finished ascending order problem

--- Main.java
+++ Main.java
@@ -1,6 +1,8 @@
+import java.util.Scanner;
+
 /**
- *
- * @author 
+ * Put two numbers in ascending order
+ * @author Aidan Cheesmond
  */
 public class Main {
 
@@ -9,7 +11,28 @@ public class Main {
    * @param args the command line arguments
    */
   public static void main(String[] args) {
-    // TODO code application logic here
+    // create a scanner for user imput
+    Scanner input = new Scanner(System.in);
     
+    // get the user's first numbers
+    System.out.println("Please enter an integer:");
+    int a = input.nextInt();
+
+    // get the user's second numbers
+    System.out.println("Please enter another integer:");
+    int b = input.nextInt();
+
+    // figure out what the ascending order is
+    if (a > b) {
+      System.out.println("Your numbers in ascending order are " + b + "," + a);
+    } else if (a < b) {
+      System.out.println("Your numbers in ascending order are " + a + "," + b);
+    } else {
+      System.out.println("Your numbers in ascending order are " + a + "," + b);
+    }
+    
+
+
+
   }
 }
